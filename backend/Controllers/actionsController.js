d2b35fead2cfb58bf668ab1e17875ce24d15bee8
Maya David <mayadd@mta.ac.il>
2024-02-21 14:18:52 +0200

    add json file of the actions

--- backend/Controllers/actionsController.js
+++ backend/Controllers/actionsController.js
@@ -1,13 +1,31 @@
 const actionService = require("../Services/ActionsService");
+const jf = require("jsonfile");
 
 const actionsCheck = async (req, res, next) => {
   const Name = req.headers["username"];
   if (!Name) return res.status(401).json({ message: "No Name provided" });
 
   const hasActions = await actionService.checkUserActions(Name);
+
   if (!hasActions) {
     return res.status(500).send("No More Actions For User!");
   } else {
+    //get user id
+    const currentDate = new Date();
+    const currentDay = currentDate.getDate();
+    const actionAllowed = user.NumOfActions - user.ActionsSoFar;
+    const newAction = {
+      id: user._id,
+      maxActions: user.maxActions,
+      date: currentDay.toString(),
+      actionAllowed: actionAllowed,
+    };
+    //get the json from the log file
+    const data = await jf.readFile("../jsonData/logs.json");
+    //add newAction to the array of logs
+    data.actions.push(newAction);
+    //write back to log file
+    await jf.writeFile("../jsonData/logs.json", data);
     next();
   }
 };
