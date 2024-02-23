65d0cdb99b10f27ccbc92a7f8fdcb8a7379a4dbc
Maya David <mayadd@mta.ac.il>
2024-02-23 13:47:52 +0200

    writhing logs to json file

--- backend/Controllers/actionsController.js
+++ backend/Controllers/actionsController.js
@@ -1,5 +1,4 @@
 const actionService = require("../Services/ActionsService");
-const jf = require("jsonfile");
 
 const actionsCheck = async (req, res, next) => {
   const Name = req.headers["username"];
@@ -10,22 +9,7 @@ const actionsCheck = async (req, res, next) => {
   if (!hasActions) {
     return res.status(500).send("No More Actions For User!");
   } else {
-    //get user id
-    const currentDate = new Date();
-    const currentDay = currentDate.getDate();
-    const actionAllowed = user.NumOfActions - user.ActionsSoFar;
-    const newAction = {
-      id: user._id,
-      maxActions: user.maxActions,
-      date: currentDay.toString(),
-      actionAllowed: actionAllowed,
-    };
-    //get the json from the log file
-    const data = await jf.readFile("../jsonData/logs.json");
-    //add newAction to the array of logs
-    data.actions.push(newAction);
-    //write back to log file
-    await jf.writeFile("../jsonData/logs.json", data);
+    await actionService.logAction(Name);
     next();
   }
 };
--- backend/Services/ActionsService.js
+++ backend/Services/ActionsService.js
@@ -1,4 +1,5 @@
 const userRepo = require("../Repositories/userRepository");
+const jf = require("jsonfile");
 
 const checkUserActions = async (Name) => {
   try {
@@ -25,7 +26,30 @@ const resetActions = async () => {
   await userRepo.resetActions();
 };
 
+const logAction = async (Name) => {
+  //get user id
+  const allUsers = await userRepo.getAllUsers();
+  const user = allUsers.find((u) => u.Name === Name);
+
+  const currentDate = new Date();
+  // const currentDay = currentDate.getDate();
+  const actionAllowed = user.NumOfActions - user.ActionsSoFar;
+  const newAction = {
+    id: user._id,
+    maxActions: user.NumOfActions,
+    date: currentDate.toLocaleDateString(),
+    actionAllowed: actionAllowed,
+  };
+  //get the json from the log file
+  const data = await jf.readFile("../backend/jsonData/logs.json");
+  //add newAction to the array of logs
+  data.actions.push(newAction);
+  //write back to log file
+  await jf.writeFile("../backend/jsonData/logs.json", data);
+};
+
 module.exports = {
   checkUserActions,
   resetActions,
+  logAction,
 };
